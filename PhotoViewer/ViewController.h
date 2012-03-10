d5cd2bd871d7dc8a3fef3e77519b0d40e7f1e285
Bryce Craig <brycemcraig@gmail.com>
2012-03-10 12:29:27 -0800

    Initial Commit

---
+++ PhotoViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoViewer
+//
+//  Created by Bryce Craig on 3/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
